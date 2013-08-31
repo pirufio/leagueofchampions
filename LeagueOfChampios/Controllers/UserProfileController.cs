964bb080c63066415787acaf40e78e498754a330
pirufio <francoandresvalle@gmail.com>
2013-08-31 13:23:04 -0300

    first commit

---
+++ LeagueOfChampios/Controllers/UserProfileController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LeagueOfChampios.Models;
+
+namespace LeagueOfChampios.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        private UsersContext db = new UsersContext();
+
+        public UserProfileController()
+        {
+            db.Configuration.ValidateOnSaveEnabled = true;
+        }
+        //
+        // GET: /UserProfile/
+
+        public ActionResult Index()
+        {
+            return View(db.UserProfiles.ToList());
+        }
+
+        //
+        // GET: /UserProfile/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+
+        //
+        // GET: /UserProfile/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /UserProfile/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UserProfile userprofile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserProfiles.Add(userprofile);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userprofile);
+        }
+
+        //
+        // GET: /UserProfile/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            userprofile.UserId = id;
+            return View(userprofile);
+        }
+
+        //
+        // POST: /UserProfile/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UserProfile userprofile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userprofile).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userprofile);
+        }
+
+        //
+        // GET: /UserProfile/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+
+        //
+        // POST: /UserProfile/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            db.UserProfiles.Remove(userprofile);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LeagueOfChampios/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LeagueOfChampios.MvcApplication" Language="C#" %>
